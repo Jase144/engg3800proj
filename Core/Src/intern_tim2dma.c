e30842bfcfe2b8f30d68c5e2819b94c02ef8d44b
s4804548 <jason.a.loadsman@gmail.com>
2025-08-09 22:44:58 +1000

    commit before dma play:

--- Core/Src/intern_tim2dma.c
+++ Core/Src/intern_tim2dma.c
@@ -4,5 +4,12 @@
 #include "intern_tim2dma.h"
 
 void reg_tim2dma_initdma_external(void) {
-    int a = 1;
+    //TIM2->CR2 |= (1 << TIM_CR2_CCDS_Pos);
+    //TIM2->DIER |= (1 << TIM_DIER_UDE_Pos);
+
+    //DMA->CCR &= ~(0x02 << DMA_CCR_PSIZE_Pos);
+    //DMA->CCR |= (0x01 << DMA_CCR_DIR_Pos);
+    //DMA->
+    //DMA->CCR |= (0x01 << DMA_CCR_EN_Pos);
+
 }
